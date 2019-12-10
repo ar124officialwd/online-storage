674cc0f129b67cee51d8248575265c0c989b3889
Ahmad Raza <ar124official2019@gmail.com>
2019-12-10 16:16:04 +0500

    Added support for win32

--- online-storage-server/extra/get-file-type.js
+++ online-storage-server/extra/get-file-type.js
@@ -1,24 +1,8 @@
-const Logger = require('../configuration/configuration').getLogger()
-const mmm = require('mmmagic')
-const Magic = mmm.Magic
-
-function getFileTypePromise(location) {
-  return new Promise((resolve, reject) => {
-    var magic = new Magic(mmm.MAGIC_MIME_TYPE);
-    magic.detectFile(location, (err, result) => {
-      if (err)
-        reject(err)
-      resolve(result)
-    })
-  })
-}
-
+const fileType = require('file-type')
+const readChunk = require('read-chunk')
 async function getFileType(location) {
-  try {
-    return await getFileTypePromise(location)
-  } catch(err) {
-    Logger.log(err)
-  }
+  const buffer = readChunk.sync(location, 0, fileType.minimumBytes)
+  return fileType(buffer).mime;
 }
 
 module.exports = getFileType;
