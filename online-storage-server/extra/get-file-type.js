edfe4278cb5245e31d493df4704399df1dba47d3
Ahmad Raza <ar124official2019@gmail.com>
2019-12-11 19:37:20 +0500

    Fix mime detection issue for unknowns

--- online-storage-server/extra/get-file-type.js
+++ online-storage-server/extra/get-file-type.js
@@ -2,7 +2,11 @@ const fileType = require('file-type')
 const readChunk = require('read-chunk')
 async function getFileType(location) {
   const buffer = readChunk.sync(location, 0, fileType.minimumBytes)
-  return fileType(buffer).mime;
+  if (buffer) {
+    return fileType(buffer).mime
+  }
+
+  return 'file/unknown'
 }
 
 module.exports = getFileType;
