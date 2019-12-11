18a3290e368eb6587ed15db207078f436e427114
Ahmad Raza <ar124official2019@gmail.com>
2019-12-11 21:34:43 +0500

    fix undefined filetype issue

--- online-storage-server/extra/get-file-type.js
+++ online-storage-server/extra/get-file-type.js
@@ -2,10 +2,10 @@ const fileType = require('file-type')
 const readChunk = require('read-chunk')
 async function getFileType(location) {
   const buffer = readChunk.sync(location, 0, fileType.minimumBytes)
-  if (buffer) {
-    return fileType(buffer).mime
+  const fileTypeObject = fileType(buffer)
+  if (fileTypeObject) {
+    return fileTypeObject.mime
   }
-
   return 'file/unknown'
 }
 
